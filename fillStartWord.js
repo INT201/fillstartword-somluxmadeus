0826fb4590dbea1c30930eba0149cb6f58dae462
somluxmadeus <73432589+somluxmadeus@users.noreply.github.com>
2022-08-30 15:52:50 +0700

    test3

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,15 +1,15 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-      if (word === undefined){
-    return 'undefined'
-    } 
-    else if(word === null){
-    return 'null'
-    }
-    else{
-    return startWord + word
-    }
-    
+  if (word === undefined){
+return undefined
+} 
+else if(word === null){
+return undefined
+}
+else{
+return startWord + word
+}
+
 }
 module.exports = fillStartWord
